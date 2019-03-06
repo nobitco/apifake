dd7638c4fa156f6bc003b6a2a7af231a8dfb2e6a
jesmarquez <jesmqz@gmail.com>
2019-03-06 09:48:55 -0500

    Change port to 5000

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const api = require('./api')
 
 require ('dotenv').config()
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 const app = express()
 const server = http.createServer(app)
 
